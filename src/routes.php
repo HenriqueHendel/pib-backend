465f67a4736d2ef90810ff95cf59777b2c79ceb6
Henrique Hendel <henriquehendell@gmail.com>
2021-07-25 20:18:12 -0300

    feat: define GET and POST Cult routes

--- src/routes.php
+++ src/routes.php
@@ -1,3 +1,27 @@
 <?php
 	include_once('./src/pdo/Connection.php');
 	include_once('./src/models/Cult.php');
+
+	$method = $_SERVER['REQUEST_METHOD'];
+
+
+	if($method === 'GET') {
+		$cult = new Cult();
+
+		$cults = $cult->getCults();
+
+
+		echo(json_encode($cults));
+	}
+
+	if ($method === 'POST') {
+		$title = $_POST['title'];
+		$data = $_POST['data'];
+		$hour = $_POST['hour'];
+		$file = $_POST['file'];
+	
+		$cult = new Cult();
+		$newCult = $cult->createCult($title, $data, $hour, $file);
+		
+		echo($newCult);
+	}
